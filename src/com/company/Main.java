fbb17ee83189648e714e0a68518b30dca06b80a7
gsandamali <gsandamali30@gmail.com>
2022-03-02 10:46:50 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+    {
+        double num = 1.0;
+    }
+    Main() {
+        var name = "Main";
+    }
+    public static void main(String[] args) {
+        var size = 7;
+        for (var i=0; i < 10; i++) {
+
+        }
+        for (var arg:args){
+
+        }
+
+        size = 4;
+        // size = "five";
+    }
+}
